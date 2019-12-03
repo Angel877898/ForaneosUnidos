9a7b585c329fe62f0ba97c01cf8cfdb4abe9e823
Angel <angurie_2008@hotmail.com>
2019-12-02 23:13:30 -0600

    Añadido Contactanos con DOM y JS, quienes somos y mejoras en index

--- blog/sobreNosotros.php
+++ blog/sobreNosotros.php
@@ -8,7 +8,11 @@
 	<h3>¿Quienes Somos?</h3>
 	
 	<p>Somos un grupo de estudiantes de la Universidad Autónoma de Queretaro presentando nuestro proyecto final de la materia Desarrollo Web.</p>
-    <p>Dado a los tiempos establecidos en el programa academico, nuestra Web se encuentra en una versión alpha y esperamos continuarla terminando el presente ciclo académico para asi contar con un proyecto mas formal.</p>
+	<p>Alumnos:</p>
+                <p>°Angel Uriel Hernandez Maldonado.</p>
+                <p>°Ismael Josetp Nuñez Baeza.</p>
+                <p>°Oscar Ceciliano.</p>
+	<p>Dado a los tiempos establecidos en el programa academico, nuestra Web se encuentra en una versión alpha y esperamos continuarla terminando el presente ciclo académico para asi contar con un proyecto mas formal.</p>
 	
 	<img src="./assets/img/escudo.png" >
 	
