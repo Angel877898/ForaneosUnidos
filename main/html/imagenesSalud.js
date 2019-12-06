83d5cf038afa4940d3a7db6b49e1954d146b6294
Angel <angurie_2008@hotmail.com>
2019-12-06 11:15:33 -0600

    final final

---
+++ main/html/imagenesSalud.js
@@ -0,0 +1,33 @@
+var images=['../imgs/correr.jpeg','../imgs/comidaCalle.jpeg','../imgs/dormir.jpeg','../imgs/salud.jpg','../imgs/hidratado.jpg'],
+    cont=0;
+
+function carrusel(imagenes){
+    imagenes.addEventListener('click',e=>{
+        let atras=imagenes.querySelector('.atras'),
+            adelante=imagenes.querySelector('.adelante'),
+            img=imagenes.querySelector('img'),
+            tgt=e.target;
+
+        if(tgt==atras){
+            if(cont>0){
+                img.src=images[cont-1];
+                cont--;
+            }else{
+                img.src=images[images.length-1];
+                cont=images.length-1;
+            }
+        }else if(tgt==adelante){
+            if(cont<images.length-1){
+                img.src=images[cont+1];
+                cont++;
+            }else{
+                img.src=images[0];
+                cont=0;
+            }
+        }
+    })
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    let imagenes=document.querySelector('.imagenes');
+    carrusel(imagenes)
+})
